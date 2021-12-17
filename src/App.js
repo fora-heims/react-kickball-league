3e70b8d278be4b4b44f14166586496cca74e4b80
fora-heims <foraheims@gmail.com>
2021-12-16 16:26:31 -0800

    cleared CRA template content. Replaced with BrowserRouter with Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Head Dur</header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/players/:id" />
+          <Route path="/players" />
+          <Route path="/teams/:id" />
+          <Route path="/teams" />
+          <Route path="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
